b45c57063a726c4120e0a07af4456606e2be6fe6
Andreas Bruckmeier <andreas.bruckmeier@web.de>
2018-03-18 08:39:17 +0100

    Admin-Area: password reset mask for the admin

--- profil/admin.py
+++ profil/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import KioskUser
-# Register your models here.
-admin.site.register(KioskUser)
+
+
+# Register
+admin.site.register(KioskUser, UserAdmin)
