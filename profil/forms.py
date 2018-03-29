dd31349fee9301a431099ca8bff10e4ff2706b14
Andreas Bruckmeier <andreas.bruckmeier@web.de>
2018-03-30 01:09:25 +0200

    Password Reset via Slack Messages

--- profil/forms.py
+++ profil/forms.py
@@ -2,7 +2,83 @@ from django import forms
 from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import UserCreationForm
 from django.utils.translation import ugettext_lazy as _
+
+from django.contrib.auth.tokens import default_token_generator
+from kiosk.bot import slack_SendMsg
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from django.template.loader import render_to_string
+
 from  .models import KioskUser
+from . import models
+
+
+
+# Slack Reset Form
+# This form is used in the template to enter the slack name to send a message to the user with a reset link to generate a new password. This form finds the user and sends the url.
+class SlackResetForm(forms.Form):
+    
+    slackName = forms.CharField(label="Slack-Name", max_length=40)
+
+
+    def send_slackMessage(self, subject_template_name, email_template_name, context, from_email, to_user, html_email_template_name=None):
+        """
+        Send a Slack message
+        """
+        subject = render_to_string(subject_template_name, context)
+        # subject *must not* contain newlines
+        subject = ''.join(subject.splitlines())
+        body = render_to_string(email_template_name, context)
+
+        msg = '*'+str(subject)+'*\n\n\r' + str(body)
+        try:
+            slack_SendMsg(msg,to_user)
+        except:
+            pass
+
+
+    def get_users(self, slackName):
+        """Given an slackName, return matching user(s) who should receive a reset.
+        This allows subclasses to more easily customize the default policies
+        that prevent inactive users and users with unusable passwords from
+        resetting their password.
+        """
+        active_users = models.KioskUser._default_manager.filter(**{
+            'slackName__iexact': slackName,
+            'is_active': True,
+        })
+        return (u for u in active_users if u.has_usable_password())
+
+    def save(self, domain_override=None, subject_template_name='registration/password_reset_subject.html', email_template_name='registration/password_reset_message.html', use_https=False, token_generator=default_token_generator, from_email=None, request=None, html_email_template_name=None, extra_email_context=None):
+        """
+        Generate a one-use only link for resetting password and send it to the
+        user.
+        """
+        slackName = self.cleaned_data["slackName"]
+        for user in self.get_users(slackName):
+            if not domain_override:
+                current_site = get_current_site(request)
+                site_name = current_site.name
+                domain = current_site.domain
+            else:
+                site_name = domain = domain_override
+            context = {
+                'slackName': slackName,
+                'domain': domain,
+                'site_name': site_name,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)).decode(),
+                'user': user,
+                'token': token_generator.make_token(user),
+                'protocol': 'https' if use_https else 'http',
+                **(extra_email_context or {}),
+            }
+            print('send')
+            self.send_slackMessage(
+                subject_template_name, email_template_name, context, from_email,
+                user, html_email_template_name=html_email_template_name,
+            )
+
 
 
 class UserErstellenForm(UserCreationForm):
--- profil/urls.py
+++ profil/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
 from django.contrib.auth import views
+
 from . import views as profilViews
+from . import forms
+
+
+
 
 urlpatterns = [
     
@@ -10,7 +15,7 @@ urlpatterns = [
     url(r'^accounts/password/change/$', views.PasswordChangeView.as_view(template_name='registration/password_change.html'), name='password_change'),
     url(r'^accounts/password/change/done/$', views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), name='password_change_done'),
 
-    url(r'^accounts/password/reset/$', views.PasswordResetView.as_view(template_name='registration/password_reset.html'), name='password_reset'),
+    url(r'^accounts/password/reset/$', views.PasswordResetView.as_view(template_name='registration/password_reset_form.html', form_class=forms.SlackResetForm, email_template_name='registration/password_reset_message.html', subject_template_name='registration/password_reset_subject.html', ), name='password_reset'),
     url(r'^accounts/password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
     url(r'^accounts/password/reset/done/$', views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
     url(r'^accounts/password/reset/complete/$', views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
