aa66f056f53cec475379798640e7844da1dc3079
Andreas Bruckmeier <abruckmeier@ffe.de>
2024-08-06 23:45:07 +0200

    Comment out logging settings for now

--- ffeKiosk/settings.py
+++ ffeKiosk/settings.py
@@ -203,7 +203,7 @@ file_handlers = {
     },
 }
 
-LOGGING = {
+"""LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'formatters': {
@@ -229,5 +229,4 @@ LOGGING = {
             'level': 'INFO',
         },
     },
-}
-
+}"""
