0523f79e0fabe9bd67b408e9b7685d66eb7c8d4f
Andreas Bruckmeier <abruckmeier@ffe.de>
2024-08-06 23:33:22 +0200

    Change build file

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,13 @@
-pipenv requirements > requirements.txt
-pip install -r requirements.txt
-python3.12 manage.py migrate
-python3.12 manage.py collectstatic
+#!/bin/bash
+
+# Build the project
+echo "Building the project..."
+python3.12 -m pip install pipenv
+python3.12 -m pipenv requirements > requirements.txt
+python3.12 -m pip install -r requirements.txt
+
+echo "Make Migration..."
+python3.12 manage.py migrate --noinput
+
+echo "Collect Static..."
+python3.12 manage.py collectstatic --noinput --clear
