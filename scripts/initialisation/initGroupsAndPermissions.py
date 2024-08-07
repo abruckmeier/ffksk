3ea79a4f364742f6359c53c781936eaef32ca1b4
Andreas Bruckmeier <abruckmeier@ffe.de>
2024-08-07 20:52:51 +0200

    Bug Fix for creating permissions for groups

--- scripts/initialisation/initGroupsAndPermissions.py
+++ scripts/initialisation/initGroupsAndPermissions.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
 '''
-	Initialise the Groups and Permissions after the models have been initialised in the database.
-	Permissions are initialised with the models itself.
+    Initialise the Groups and Permissions after the models have been initialised in the database.
+    Permissions are initialised with the models itself.
 
-	Do the assignment of groups here, not in the Admin-Surface!
+    Do the assignment of groups here, not in the Admin-Surface!
 '''
 
 
@@ -13,11 +13,11 @@ import sys
 import django
 
 if __name__ == '__main__':
-	# Setup the Django environment of the Kiosk
-	BASE = os.path.dirname(os.path.dirname(os.path.dirname((os.path.abspath(__file__)))))
-	sys.path.append(BASE)
-	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ffeKiosk.settings")
-	django.setup()
+    # Setup the Django environment of the Kiosk
+    BASE = os.path.dirname(os.path.dirname(os.path.dirname((os.path.abspath(__file__)))))
+    sys.path.append(BASE)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ffeKiosk.settings")
+    django.setup()
 
 
 # Import the Modules for the tasks
@@ -26,135 +26,133 @@ from django.contrib.auth.models import Group, Permission
 
 
 def GetOrCreateGroup(name):
-	try:
-		grp = Group.objects.get(name=name)
-	except:
-		grp = Group(name=name)
-		grp.save()
-	return grp
+    try:
+        grp = Group.objects.get(name=name)
+    except:
+        grp = Group(name=name)
+        grp.save()
+    return grp
 
 
 # User functions here (or from import)
 def InitGroupsAndPermissions():
 
-	# -----------
-	# Admin
-	#	But not superuser
-	# -----------
-	admin = GetOrCreateGroup(name='Admin')
+    # -----------
+    # Admin
+    #    But not superuser
+    # -----------
+    admin = GetOrCreateGroup(name='Admin')
 
-	p = Permission.objects.none()
+    p = Permission.objects.none()
 
-	p |= Permission.objects.filter(
-		content_type__app_label='kiosk', 
-		content_type__model='einkaufsliste', 
-		codename__iregex=r'(add|change|delete)_einkaufsliste',
-	)
-	p |= Permission.objects.filter(
-		content_type__app_label='kiosk', 
-		content_type__model='einkaufslistegroups', 
-		codename__iregex=r'(add|change|delete)_einkaufslistegroups',
-	)
-	p |= Permission.objects.filter(
-		content_type__app_label='kiosk', 
-		content_type__model='kioskkapazitaet', 
-		codename__iregex=r'(add|change)_kioskkapazitaet',
-	)
-	p |= Permission.objects.filter(
-		content_type__app_label='kiosk', 
-		content_type__model='kontakt_nachricht', 
-		codename='change_kontakt_nachricht',
-	)
-	p |= Permission.objects.filter(
-		content_type__app_label='kiosk', 
-		content_type__model='produktpalette', 
-		codename__iregex=r'(add|change)_produktpalette',
-	)
-	p |= Permission.objects.filter(
-		content_type__app_label='kiosk', 
-		content_type__model='start_news', 
-		codename__iregex=r'(add|change)_start_news',
-	)
-	p |= Permission.objects.filter(
-		content_type__app_label='kiosk', 
-		content_type__model='produktverkaufspreise', 
-		codename='add_produktverkaufspreise',
-	)
-	p |= Permission.objects.filter(
-		content_type__app_label='profil', 
-		content_type__model='kioskuser', 
-		codename__iregex=r'(add|change)_kioskuser',
-	)
-	p |= Permission.objects.filter(
-		content_type__app_label='profil', 
-		content_type__model='kioskuser', 
-		codename='do_admin_tasks',
-	)
-	p |= Permission.objects.filter(
-		content_type__app_label='paypal',
-		content_type__model='mail',
-		codename='change_kioskuser',
-	)
-	for pp in p:
-		admin.permissions.add(pp)
-
-
-	# -----------
-	# Verwalter
-	# -----------
-	verwalter = GetOrCreateGroup(name='Verwalter')
-
-	p = Permission.objects.none()
-	p |= Permission.objects.filter(
-		content_type__app_label='profil', 
-		content_type__model='kioskuser', 
-		codename='do_verwaltung',
-	)
-	for pp in p:
-		admin.permissions.add(pp)
+    p |= Permission.objects.filter(
+        content_type__app_label='kiosk', 
+        content_type__model='einkaufsliste', 
+        codename__iregex=r'(add|change|delete)_einkaufsliste',
+    )
+    p |= Permission.objects.filter(
+        content_type__app_label='kiosk', 
+        content_type__model='einkaufslistegroups', 
+        codename__iregex=r'(add|change|delete)_einkaufslistegroups',
+    )
+    p |= Permission.objects.filter(
+        content_type__app_label='kiosk', 
+        content_type__model='kioskkapazitaet', 
+        codename__iregex=r'(add|change)_kioskkapazitaet',
+    )
+    p |= Permission.objects.filter(
+        content_type__app_label='kiosk', 
+        content_type__model='kontakt_nachricht', 
+        codename='change_kontakt_nachricht',
+    )
+    p |= Permission.objects.filter(
+        content_type__app_label='kiosk', 
+        content_type__model='produktpalette', 
+        codename__iregex=r'(add|change)_produktpalette',
+    )
+    p |= Permission.objects.filter(
+        content_type__app_label='kiosk', 
+        content_type__model='start_news', 
+        codename__iregex=r'(add|change)_start_news',
+    )
+    p |= Permission.objects.filter(
+        content_type__app_label='kiosk', 
+        content_type__model='produktverkaufspreise', 
+        codename='add_produktverkaufspreise',
+    )
+    p |= Permission.objects.filter(
+        content_type__app_label='profil', 
+        content_type__model='kioskuser', 
+        codename__iregex=r'(add|change)_kioskuser',
+    )
+    p |= Permission.objects.filter(
+        content_type__app_label='profil', 
+        content_type__model='kioskuser', 
+        codename='do_admin_tasks',
+    )
+    p |= Permission.objects.filter(
+        content_type__app_label='paypal',
+        content_type__model='mail',
+        codename='change_kioskuser',
+    )
+    for pp in p:
+        admin.permissions.add(pp)
 
 
-	# -----------
-	# Einkäufer
-	# -----------
-	einkaufer = GetOrCreateGroup(name='Einkaufer')
+    # -----------
+    # Verwalter
+    # -----------
+    verwalter = GetOrCreateGroup(name='Verwalter')
 
-	p = Permission.objects.none()
-	p |= Permission.objects.filter(
-		content_type__app_label='profil', 
-		content_type__model='kioskuser', 
-		codename='do_einkauf',
-	)
-	for pp in p:
-		admin.permissions.add(pp)
-
-
-	# -----------
-	# Nutzer
-	# -----------
-	nutzer = GetOrCreateGroup(name='Nutzer')
-
-	p = Permission.objects.none()
-	p |= Permission.objects.filter(
-		content_type__app_label='profil', 
-		content_type__model='kioskuser', 
-		codename='perm_kauf',
-	)
-	for pp in p:
-		admin.permissions.add(pp)
+    p = Permission.objects.none()
+    p |= Permission.objects.filter(
+        content_type__app_label='profil', 
+        content_type__model='kioskuser', 
+        codename='do_verwaltung',
+    )
+    for pp in p:
+        verwalter.permissions.add(pp)
 
 
+    # -----------
+    # Einkäufer
+    # -----------
+    einkaufer = GetOrCreateGroup(name='Einkaufer')
 
-	print('Groups created and permissions assigned.')
+    p = Permission.objects.none()
+    p |= Permission.objects.filter(
+        content_type__app_label='profil', 
+        content_type__model='kioskuser', 
+        codename='do_einkauf',
+    )
+    for pp in p:
+        einkaufer.permissions.add(pp)
 
-	return
+
+    # -----------
+    # Nutzer
+    # -----------
+    nutzer = GetOrCreateGroup(name='Nutzer')
+
+    p = Permission.objects.none()
+    p |= Permission.objects.filter(
+        content_type__app_label='profil', 
+        content_type__model='kioskuser', 
+        codename='perm_kauf',
+    )
+    for pp in p:
+        nutzer.permissions.add(pp)
+
+    print('Groups created and permissions assigned.')
+    return
 
 
 # Run the Script
 
 if __name__ == '__main__':
 
-	# Permissions are initialised with the models itself.
+    # Permissions are initialised with the models itself.
 
-	# Now, Init the groups and concatenate the permissions
+    # Now, Init the groups and concatenate the permissions
     InitGroupsAndPermissions()
+    
