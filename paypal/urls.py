21e0aac3624e72b80aefa81314f5989fec6af355
Andreas Bruckmeier <abruckmeier@ffe.de>
2024-08-20 22:42:23 +0200

    More crosslinks + Instructions for money deposit on Kiosk account

--- paypal/urls.py
+++ paypal/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     re_path(r'sync/?$',
             views.SyncPayPalTransactions.as_view(),
             name='paypal_sync'),
+    re_path(r'einzahlung-info/?$',
+            views.PayPalEinzahlungInfoPage.as_view(),
+            name='paypal_einzahlung_page'),
 ]
