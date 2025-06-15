cea496586a37f9fb626cd2e4508323f6d09fae1f
Andreas Bruckmeier <abruckmeier@ffe.de>
2025-06-15 10:21:18 +0200

    Bug Fix

--- paypal/offline_token.py
+++ paypal/offline_token.py
@@ -10,6 +10,9 @@ from django.conf import settings
 from django.http import HttpRequest, HttpResponseRedirect
 from django.urls import reverse
 from google_auth_oauthlib.flow import InstalledAppFlow
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def obtain_token(
@@ -62,12 +65,14 @@ def gmail_auth_response(request: HttpRequest) -> HttpResponseRedirect | None:
     """
 
     response_uri = request.build_absolute_uri()
-    response_uri = response_uri.replace('http', 'https')
+    response_uri = response_uri.replace('http://', 'https://')
+    logger.warning(response_uri)
 
     flow = InstalledAppFlow.from_client_config(settings.OAUTH_CREDENTIALS, settings.OAUTH_SCOPES)
     flow.redirect_uri = request.build_absolute_uri(reverse('gmail_auth_response_page'))
 
     flow.fetch_token(authorization_response=response_uri)
+    # oauthlib.oauth2.rfc6749.errors.InsecureTransportError: (insecure_transport) OAuth 2 MUST utilize https.
 
     with open('token.json', 'w') as token_file:
         token_file.write(flow.credentials.to_json())
