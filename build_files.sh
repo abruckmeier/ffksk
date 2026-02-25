e116642dd4a736f37c4cf4b423cd431305852168
Andreas Bruckmeier <abruckmeier@ffe.de>
2026-02-25 22:54:20 +0100

    Use uvx

--- build_files.sh
+++ build_files.sh
@@ -2,16 +2,14 @@
 
 # Build the project
 echo "Building the project..."
-uv pip install pipenv
-uv run python -m pipenv requirements > requirements.txt
-uv pip install -r requirements.txt
+uvx pipenv install --deploy --ignore-pipfile
 
 echo "Make Migration..."
-uv run python manage.py migrate --noinput
+uvx pipenv run python manage.py migrate --noinput
 
 echo "Collect Static..."
-uv run python manage.py collectstatic --noinput --clear
+uvx pipenv run python manage.py collectstatic --noinput --clear
 
 echo "Setup groups and their permissions"
-uv run python manage.py shell -c "from scripts.initialisation import initGroupsAndPermissions as x; x.InitGroupsAndPermissions();"
-uv run python manage.py shell -c "from scripts.initialisation import initNecessaryUsers as x; x.initNecessaryUsers();"
+uvx pipenv run python manage.py shell -c "from scripts.initialisation import initGroupsAndPermissions as x; x.InitGroupsAndPermissions();"
+uvx pipenv run python manage.py shell -c "from scripts.initialisation import initNecessaryUsers as x; x.initNecessaryUsers();"
