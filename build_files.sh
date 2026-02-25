578f0bb66650b1d97395b4d093b5d7ee72a78183
Andreas Bruckmeier <abruckmeier@ffe.de>
2026-02-25 23:03:48 +0100

    Try to use pregenerated requirements.txt

--- build_files.sh
+++ build_files.sh
@@ -3,7 +3,6 @@
 # Build the project
 echo "Building the project..."
 uvx pipenv install --deploy --ignore-pipfile
-uvx pipenv requirements > requirements.txt
 
 echo "Make Migration..."
 uvx pipenv run python manage.py migrate --noinput
